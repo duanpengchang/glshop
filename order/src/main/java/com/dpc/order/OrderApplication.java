a809d7b8f846bf8d229fac2c54970a69be32d50d
duanpengchang <363739008@qq.com>
2022-01-18 23:03:24 +0800

    商城初始化

---
+++ order/src/main/java/com/dpc/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.dpc.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+ public static void main(String[] args) {
+  SpringApplication.run(OrderApplication.class, args);
+ }
+
+}
